e936b1acdd216c760e7bdb3880d8d8ebba2ae86f
sejinahmedph <sahmedprogrammer@gmail.com>
2023-12-19 22:20:44 +0600

    added heroes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Images
 import banner from './images/banner-bg.png';
 import logo from './images/logo.svg';
+import ic_menu from './images/ic_menu.svg';
+import jhankarMahbub from './images/ceo.png';
+import masudRana from './images/senior-web-developer.png';
+import sejinAhmed from './images/junior-web-developer.png';
+// Bootstrap Components
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button  from 'react-bootstrap/Button';
 import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 
 function Home() {
@@ -21,43 +30,41 @@ function Home() {
     <div>
       <main>
         <header id='header'>
-          
-
-          
-
-    <Navbar expand="lg" className="">
-      <Container>
-        <img src={logo} height={'66px'} width={'66px'} alt="logo-phero"/>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto text-center gap-4">
-            <Nav.Link className='text-white' href="/Level-2">Level 2</Nav.Link>
-            <Nav.Link className='text-white' href="/Course-details">Course Details</Nav.Link>
-            <Nav.Link className='text-white' href="/About">About Us</Nav.Link>
-            <Nav.Link className='text-white' href="/Login">Login</Nav.Link>
-            <Button className='btn' onClick={handleShow}>Enroll Now</Button>
-            <Modal show={lgShow} onHide={handleClose} backdrop="static" keyboard={false}>
-              <Modal.Header closeButton>
-                <Modal.Title>Modal title</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                I will not close if you click outside me. Do not even try to press
-                escape key.
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
-                <Button variant="primary">Understood</Button>
-              </Modal.Footer>
-            </Modal>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    
-
-
+          <Navbar expand="lg">
+            <Container>
+              <div className="d-flex align-items-center">
+                <img src={logo} height={'66px'} width={'66px'} style={{marginTop: "-7px"}} alt="logo-phero"/>
+                <span className="fs-5 text-white" style={{marginLeft: "-15px"}}>Programming Hero</span>
+              </div>
+              <Navbar.Toggle aria-controls="basic-navbar-nav">
+                <img src={ic_menu} alt="ic_menu"/>
+              </Navbar.Toggle>
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto text-center gap-4">
+                  <Nav.Link className='text-white' href="/Level-2">Level 2</Nav.Link>
+                  <Nav.Link className='text-white' href="/Course-details">Course Details</Nav.Link>
+                  <Nav.Link className='text-white' href="/About">About Us</Nav.Link>
+                  <Nav.Link className='text-white' href="/Login">Login</Nav.Link>
+                  <Button className='btn ms-auto me-auto' style={{width: "120px"}} onClick={handleShow}>Enroll Now</Button>
+                  <Modal show={lgShow} onHide={handleClose} backdrop="static" keyboard={false}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Modal title</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      I will not close if you click outside me. Do not even try to press
+                      escape key.
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        Close
+                      </Button>
+                      <Button variant="primary">Understood</Button>
+                    </Modal.Footer>
+                  </Modal>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
         </header>
         <section id='banner-section'>
           <div id='banner'>
@@ -67,8 +74,50 @@ function Home() {
           <img src={banner} width={'100%'} id='banner-bg' alt="banner-bg"/>
         </section>
       </main>
-      <section>
-        <h1>sdighdshuh</h1>
+      <section id='hero-section'>
+        <div className='text-center ms-auto me-auto' style={{maxWidth: "850px"}}>
+          <h1 className='title'>Heroes In Top Companies_</h1>
+          <span>We can not guarantee you a job/intern. However, 70-80% of the students who finish our main course 
+          and SCIC on time get a job or an internship within 3-6 months of their completion.</span>
+        </div>
+        <div className='text-center py-5'>
+          <Container>
+            <Row className='gap-2'>
+              <Col>
+                <img src={jhankarMahbub} className='rounded' height={"300px"} width={"260px"} 
+                style={{background: `url(${logo}) center bottom / 500px no-repeat`}} alt="hero"/>
+                <div className='py-2'>
+                  <span className='fs-4 title-color'>Jhankar Mahbub</span><br/>
+                  <span className='title-color'>Senior Web Developer - CEO</span>
+                </div>
+              </Col>
+              <Col>
+                <img src={masudRana} className='rounded' height={"300px"} width={"260px"} 
+                style={{background: `url(${logo}) center bottom / 500px no-repeat`}} alt="hero"/>
+                <div className='py-2'>
+                  <span className='fs-4 title-color'>Masud Rana</span><br/>
+                  <span className='title-color'>Senior Web Developer - IT</span>
+                </div>
+              </Col>
+              <Col>
+                <img src={sejinAhmed} className='rounded' height={"300px"} width={"260px"} 
+                style={{background: `url(${logo}) center bottom / 500px no-repeat`}} alt="hero"/>
+                <div className='py-2'>
+                  <span className='fs-4 title-color'>Sejin Ahmed</span><br/>
+                  <span className='title-color'>Junior Web Developer</span>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </section>
+      <section id='impact-section'>
+        <Container>
+          <h1 className='title text-end'>Our Impact_</h1>
+          <article className='p-5 rounded-3' id='impact-card'>
+            frghf
+          </article>
+        </Container>
       </section>
     </div>
   
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import app from './firebase.init';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged, signOut} from 'firebase/auth';
+
 // auth
 const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // document.getElementById('sign-in-btn').style.display = 'none';
+    // document.getElementById('sign-out-btn').style.display = 'block';
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <img src="${user.photoURL}" height="50px" width="50px"><br>
+      <h1>${user.displayName}</h1>
+      <p>${user.email}</p>
+    `;
+    document.body.appendChild(div);
+    console.log('user signed in')
+    console.log(user)
+  } 
+  else {
+    // document.getElementById('sign-in-btn').style.display = 'block';
+    // document.getElementById('sign-out-btn').style.display = 'none';
+    console.log("user isn't signed in")
+  }
+});
+
+
+// handle sign out
+const handleSignOut = () => {
+  try {
+    signOut(auth).then(() => console.log("signout"))
+  } catch (error) {
+    console.log('Error logging out: ', error)
+  }
+}
+
+
 // function
 function Register() {
     // provider
@@ -15,22 +49,17 @@ function Register() {
         .then(result => {
             const data = result.user;
 
-            const div = document.createElement('div');
-            div.style.color = "white";
-            div.innerHTML = `
-              <img src="${data.photoURL}" height="100px">
-              <h1>${data.displayName}</h1>
-              <h4>${data.email}</h4>
-            `;
-            document.body.appendChild(div);
+            console.log(data)
         })
-        .catch(error => console.log(error))
+        .catch(error => console.log(error.messege))
     }
   return (
     <div>
         <h1>Register Now</h1>
         
-        <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        <button onClick={handleGoogleSignIn} id='sign-in-btn'>Google Sign In</button>
+
+        <button onClick={handleSignOut} id='sign-out-btn'>Sign Out</button>
     </div>
   );
 }
